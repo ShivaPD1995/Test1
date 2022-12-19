0f1dac44053c7a6b3e6fca4611ebdad9cac3d7fb
ShivaPD195 <shivakumardhulehole@gmail.com>
2022-12-19 22:38:38 +0530

    This is my new commit

---
+++ src/test/java/Objectclasses/Punter3.java
@@ -0,0 +1,13 @@
+package Objectclasses;
+class Punter3 
+{
+	public String toString()
+	{
+	return "I Love You";	
+	}
+	public static void main(String[] args)
+	{
+	Punter3 p =new Punter3();
+	System.out.println(p.toString());
+	}
+}
